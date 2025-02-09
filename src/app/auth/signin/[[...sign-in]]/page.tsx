edb692d28a807a421e31bcc3fc486c59bea823dc
Arnav Singhal <arnavsinghal06@gmail.com>
2025-02-09 13:10:04 +0530

    configure signin clerk

--- src/app/auth/signin/[[...sign-in]]/page.tsx
+++ src/app/auth/signin/[[...sign-in]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function SignupPage() {
+export default function SignInPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-100 via-blue-50 to-white items-center space-y-8 justify-center p-4">
       <div className="text-4xl font-bold">Sign In</div>
--- src/app/auth/signup/[[...sign-up]]/page.tsx
+++ src/app/auth/signup/[[...sign-up]]/page.tsx
@@ -1,6 +1,6 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function SignupPage() {
+export default function SignUpPage() {
   return (
     <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-100 via-blue-50 to-white items-center space-y-8 justify-center p-4">
       <div className="text-4xl font-bold">Sign Up</div>
