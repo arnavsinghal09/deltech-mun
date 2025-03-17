4d6bea0571aa5eac01b6b07ac2f0a44fe3a4c47a
Arnav Singhal <arnavsinghal06@gmail.com>
2025-03-17 17:12:43 +0530

    add conference pictures

--- src/components/custom/DelTechImageCarousel.tsx
+++ src/components/custom/DelTechImageCarousel.tsx
@@ -5,16 +5,14 @@ import Carousel from "../ui/carousel";
 export function DelTechImageCarousel() {
   const slideData = [
     {
-      src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://mun-website-images.s3.ap-south-1.amazonaws.com/mun-pics/IMG_8713.jpg",
     },
     {
-      src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://mun-website-images.s3.ap-south-1.amazonaws.com/mun-pics/DSC08400.jpg",
     },
+
     {
-      src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    },
-    {
-      src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      src: "https://mun-website-images.s3.ap-south-1.amazonaws.com/mun-pics/DSC08044.jpg",
     },
   ];
   return (
